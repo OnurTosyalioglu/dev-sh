3e602746a4550b9c6af46be9b5ae313163cd84a5
onurtosyalioglu <onurtosyalioglu@gmail.com>
2020-06-02 14:35:25 +0300

    tools scripts

---
+++ tools/tools.sh
@@ -0,0 +1,33 @@
+apt-get install -y \
+build-essential \
+dos2unix \
+gcc \
+git \
+git-lfs \
+libmcrypt4 \
+libpcre3-dev \ 
+libpng-dev \
+chrony \
+unzip \
+make \
+python2.7-dev \
+python-pip \
+python-pip3 \
+re2c \
+supervisor \
+unattended-upgrades \
+whois \ 
+vim \
+pv \
+cifs-utils \
+mcrypt \
+bash-completion \
+zsh \
+wget \
+curl \
+unzip \
+zip \
+tmux \
+zsh \
+nmap 
+
